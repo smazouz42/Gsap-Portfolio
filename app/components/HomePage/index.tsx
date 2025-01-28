659effd2a31f40fcca2b0f18c82926539ef8e6b1
smazouz42 <smazouz@student.1337.ma>
2025-01-28 15:17:28 +0100

    feat: work in progress

--- app/components/HomePage/index.tsx
+++ app/components/HomePage/index.tsx
@@ -57,13 +57,18 @@ const VideoSection = () => {
     });
 
     gsap.fromTo('.title',
-      { y: '100%', opacity: 0 },
-      { y: '0%', opacity: 1, duration: 1.5, delay: 0.9, scrollTrigger: { start: 1 } }
+      { scale:0 },
+      { scale:1, duration: 1.5, delay: 0.9, scrollTrigger: { start: 1 } }
     );
 
     gsap.fromTo('.description',
       { y: '50%', opacity: 0 },
       { y: '0%', opacity: 1, duration: 1.5, delay: 1.2, scrollTrigger: { start: 1 } }
+    );
+
+    gsap.fromTo('.photo-container',
+      { scale:0 },
+      { scale:1, duration: 1.5, delay: 1.2, scrollTrigger: { start: 1 } }
     );
 
     gsap.to('.border-segment', {
@@ -73,56 +78,64 @@ const VideoSection = () => {
       repeat: -1,
     });
 
+    
 
   }, []);
 
   return (
     <section className="video-section relative h-screen w-screen font-fira overflow-hidden">
-      <div className="relative h-full w-full -z-20 flex items-center justify-center">
-        <div className="w-full  md:w-1/2 pr-8">
-          <div className="overflow-hidden">
-            <h1 className="title text-4xl md:text-6xl font-bold mb-6">
-              Said Mazouz
-            </h1>
+      <div className="relative h-full w-full -z-20 flex items-center justify-center flex-col">
+        <div className='w-full px-10 flex'>
+          <div className="w-full  md:w-1/2">
+            <div className="overflow-hidden">
+              <h1 className="title text-4xl md:text-6xl font-bold mb-6">
+                Said Mazouz
+              </h1>
+            </div>
+            <div className="overflow-hidden">
+              <p className="description text-xl text-gray-400 ">
+                A passionate developer dedicated to creating beautiful and functional web experiences.
+                With expertise in modern web technologies, I bring ideas to life through clean code and
+                innovative solutions.
+              </p>
+            </div>
           </div>
-          <div className="overflow-hidden">
-            <p className="description text-xl text-gray-600 ">
-              A passionate developer dedicated to creating beautiful and functional web experiences.
-              With expertise in modern web technologies, I bring ideas to life through clean code and
-              innovative solutions.
-            </p>
-          </div>
-        </div>
 
-        <div className="hidden md:block w-1/2">
-          <div className="photo-container relative w-[400px] h-[400px] mx-auto">
-            <div className="absolute inset-[-10px] rounded-full">
-              <div className="border-segment absolute inset-0 border-4 border-black rounded-full"
-                style={{
-                  clipPath: `polygon(
+          <div className="hidden md:block w-1/2">
+            <div className="photo-container relative w-[400px] h-[400px] mx-auto">
+              <div className="absolute inset-[-10px] rounded-full">
+                <div className="border-segment absolute inset-0 border-4 border-black rounded-full bg-cyan-500"
+                  style={{
+                    clipPath: `polygon(
                       0 0, 40% 0, 40% 40%,
                       60% 40%, 60% 0, 100% 0,
                       100% 60%, 60% 60%,
                       60% 100%, 40% 100%,
                       40% 60%, 0 60%
                     )`,
-                  transform: 'rotate(45deg)'
-                }}>
+                    transform: 'rotate(45deg)'
+                  }}>
+                </div>
               </div>
-            </div>
 
-            <div className="absolute inset-[0px] rounded-full overflow-hidden">
-              <img
-                src="/my-picture.jpg"
-                alt="Profile Photo"
-                className="w-full h-full object-cover"
-              />
+              <div className="absolute inset-[0px] rounded-full overflow-hidden">
+                <img
+                  src="/my-picture.jpg"
+                  alt="Profile Photo"
+                  className="w-full h-full object-cover"
+                />
+              </div>
             </div>
           </div>
         </div>
 
+        <div className="flex items-center gap-4">
+          
+        </div>
 
-        <div className="absolute inset-0 -z-30 bg-black bg-opacity-70">
+
+        <div className="absolute inset-0 -z-30">
+          <div className="absolute inset-0 bg-black bg-opacity-80" />
           <video
             className="object-cover h-full w-full opacity-100"
             src="/Binary-Code.mp4"
