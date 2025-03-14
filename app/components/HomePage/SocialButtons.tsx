b1f7d690d1a0cf1d2b77811c5fbe5d756ec4306b
smazouz42 <smazouz@student.1337.ma>
2025-03-14 04:49:57 +0000

    feat: add LinkedIn link to SocialButtons component

--- app/components/HomePage/SocialButtons.tsx
+++ app/components/HomePage/SocialButtons.tsx
@@ -7,12 +7,16 @@ const SocialButtons = () => {
     <div className='social-section flex flex-col gap-6 items-center'>
       <h1 className="md:text-4xl text-3xl font-bold social-button">Connect with Me</h1>
       <div className="flex items-center gap-4">
-        <button className='bg-cyan-800 py-1 px-2 rounded-2xl social-button hover:px-6 transition-all duration-300'>
-          <FontAwesomeIcon icon={faGithub} size="2x" className="text-white" />
-        </button>
-        <button className='bg-cyan-800 py-1 px-2 rounded-2xl social-button hover:px-6 transition-all duration-300'>
-          <FontAwesomeIcon icon={faLinkedin} size="2x" className="text-white" />
-        </button>
+        <a href="https://github.com/smazouz42" target="_blank" rel="noopener noreferrer">
+          <button className='bg-cyan-800 py-1 px-2 rounded-2xl social-button hover:px-6 transition-all duration-300'>
+            <FontAwesomeIcon icon={faGithub} size="2x" className="text-white" />
+          </button>
+        </a>
+        <a href="https://www.linkedin.com/in/smazouz/" target="_blank" rel="noopener noreferrer">
+          <button className='bg-cyan-800 py-1 px-2 rounded-2xl social-button hover:px-6 transition-all duration-300'>
+            <FontAwesomeIcon icon={faLinkedin} size="2x" className="text-white" />
+          </button>
+        </a>
         <button className='bg-cyan-800 py-1 px-2 rounded-2xl social-button hover:px-6 transition-all duration-300'>
           <FontAwesomeIcon icon={faTelegram} size="2x" className="text-white" />
         </button>
